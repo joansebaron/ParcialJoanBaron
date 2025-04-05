324d9c6c1a34447fba10e6c04b2b0d6518ca80d5
Joan Baron <joansebaron@gmail.com>
2025-04-04 20:11:49 -0500

    first commit - webserver

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*function expression */
+import app from "./app.js";
+const main=() =>{
+app.listen(app.get("port"))
+console.log(`The company's super web server is running on port ${app.get("port")}`);
+
+}
+
+main();
